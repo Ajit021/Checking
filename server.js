d867ffa3d0f2e6bd9b1f5bc2431d7764a136e0c4
Ajit Kumar Singh <Ajitkumar022@gmail.com>
2023-02-27 09:51:08 +0530

    All the Changes has done

--- server.js
+++ server.js
@@ -3,21 +3,30 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path')
 const fs = require('fs')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { json } = require('express');
+const validator=require('validator');
+
+
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://127.0.0.1:27017/localdb')
 .then(()=>console.log("Successfull"))
 .catch((err)=>console.log(err));
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 //Schema for mock test
 const mocktestSchema=new mongoose.Schema({
-    Question:String,
+    Question:{
+        type:String,
+        required:true,
+        unique:true
+    },
     Options:[String],
     Correct_Answer:String,
     Subject:String
-
 })
 
 //model for mocktest
@@ -25,21 +34,21 @@ const mocktestSchema=new mongoose.Schema({
 const Mocktest=new mongoose.model("Mocktest",mocktestSchema);
 
 //add document for Mock test
-const createdocumentmocktest=async()=>{
-    try{
-        const recetmocktest= new Mocktest({
-            Question:"Who is Deepak ",
-            Options:["MAn","Boy","Girl","Other"],
-            Correct_Answer:"Man",
-            Subject:"History"
-        })
-        const result= await recetmocktest.save();
-        console.log(result)
-    }catch(err){
-        console.log(err)
-    }
+// const createdocumentmocktest=async()=>{
+//     try{
+//         const recetmocktest= new Mocktest({
+//             Question:"Who is Deepak ",
+//             Options:["MAn","Boy","Girl","Other"],
+//             Correct_Answer:"Man",
+//             Subject:"History"
+//         })
+//         const result= await recetmocktest.save();
+//         console.log(result)
+//     }catch(err){
+//         console.log(err)
+//     }
    
-}
+// }
 
 //createdocumentmocktest();
 
@@ -48,37 +57,40 @@ const createdocumentmocktest=async()=>{
 
 //Schema for previous
 const previousSchema=new mongoose.Schema({
-    Question:String,
+    Question:{
+        type:String,
+        required:true,
+        unique:true
+    },
     Options:[String],
     Correct_Answer:String,
     Year:{
-        type:String,
+        type:Number,
         required:true
+
     }
 
 })
 
 //model
-
 const Previoustest=new mongoose.model("Previoustest",previousSchema);
 
 //add document for Mock test
-const createdocumentprevious=async()=>{
-    try{
-        const recetmocktest= new Previoustest({
-            Question:"Who is Deepak ",
-            Options:["MAn","Boy","Girl","Other"],
-            Correct_Answer:"Man",
-            Year:"1999"
-        })
-        const result= await recetmocktest.save();
-        console.log(result)
-    }catch(err){
-        console.log("Failed to Save Enter coreect data")
-    }
+// const createdocumentprevious=async()=>{
+//     try{
+//         const recetmocktest= new Previoustest({
+//             Question:"Who is Deepak ",
+//             Options:["MAn","Boy","Girl","Other"],
+//             Correct_Answer:"Man",
+//             Year:"1999"
+//         })
+//         const result= await recetmocktest.save();
+//         console.log(result)
+//     }catch(err){
+//         console.log("Failed to Save Enter coreect data")
+//     }
    
-}
-
+// }
 //createdocumentprevious();
 
 
@@ -93,14 +105,84 @@ app.get("/",(req,res)=>{
 //Route to MockTest Page
 app.get("/mocktest",(req,res)=>{
     res.sendFile(__dirname+"/mocktest.html");
+    console.log("HI")
 })
 
 
 //Route to Previous Year Question  Page
 app.get("/previous",(req,res)=>{
     res.sendFile(__dirname+"/previous.html");
+    console.log("Hello")
 })
 
+
+//Post req to mock test
+
+app.post("/mocktest",async(req,res)=>{
+   try {
+
+    
+        const recetmocktest= new Mocktest({
+            Question:req.body.question.trim(),
+            Options:[req.body.optiona,req.body.optionb,req.body.optionc,req.body.optiond],
+            Correct_Answer:req.body.answer,
+            Subject:req.body.tag
+        })
+
+
+
+        const saved=await recetmocktest.save();
+
+            console.log("Saved")
+       // res.jsonp({success : true})
+        res.status(201).sendFile(__dirname+"/mocktest.html")
+      
+
+
+
+   } catch (error) {
+       res.send("Error Question Already exists")
+       console.log("Error in Putting data please check in Mocktest")
+   }
+})
+
+
+
+//Post req to previous test
+app.post("/previous",async(req,res)=>{
+    try {
+ 
+ 
+      //checking the length of the date it should be of 4 digit
+         const k=validator.isLength(req.body.date,{min:4,max:4})   
+          if(k==true){
+            const previoustest= new Previoustest({
+                Question:req.body.question.trim(),
+                Options:[req.body.optiona,req.body.optionb,req.body.optionc,req.body.optiond],
+                Correct_Answer:req.body.answer,
+                Year:req.body.date
+            })
+
+            const saved=await previoustest.save();
+            console.log("Saved")
+            //res.jsonp({success : true})
+             res.status(201).sendFile(__dirname+"/previous.html")
+         }
+        else{
+            res.send("The year length should be of 4 digit")
+        }
+         
+ 
+            
+       
+ 
+ 
+ 
+    } catch (error) {
+        res.send("Error Question Already exists")
+        console.log("Error in Putting data please check in Previous")
+    }
+ })
 
 
 
